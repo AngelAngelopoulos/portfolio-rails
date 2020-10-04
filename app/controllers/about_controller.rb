3ba9d97dbf294aa0a77ac44ac6a9b87dc21121bc
codecademy <ccuser@codecademy.com>
2020-10-04 06:18:52 +0000

    about added

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def view
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 root 'pages#welcome'
 get '/portfolio' => 'projects#index', as: :portfolio
+get '/about' => 'about#view', as: :about
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
